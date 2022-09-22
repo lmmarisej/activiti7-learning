35171cb46215197dc26f3e1ea1986e76db099f3f
lmmarisej <lmmarise@foxmail.com>
2022-09-22 23:14:33 +0800

    Add . 依赖web、jdbc、postgresql、Mybatis

---
+++ src/main/java/org/lmmarise/activiti/activiti7/Activiti7LearningApplication.java
@@ -0,0 +1,13 @@
+package org.lmmarise.activiti.activiti7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Activiti7LearningApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Activiti7LearningApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/org/lmmarise/activiti/activiti7/Activiti7LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lmmarise.activiti.activiti7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Activiti7LearningApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
